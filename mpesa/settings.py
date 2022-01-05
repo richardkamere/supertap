31a0c108f646d399a1f472f83b13ee47e9e4a8ac
Richard <richardkamere20@gmail.com>
2022-01-05 15:22:38 +0300

    update on statics
    version 0.0.3

--- mpesa/settings.py
+++ mpesa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k7$6^wx5%=gqy(8hs(^38n_3+))llx7k^sp5-)&s+^*%3p6epp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','3a1b-197-232-34-48.ngrok.io']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
