3d4c8a5df7fa6db851f27588ace4089b09387c5a
Richard <richardkamere20@gmail.com>
2022-01-06 15:29:46 +0300

    version 0.1.5
    made admin the default website

--- mpesa/urls.py
+++ mpesa/urls.py
@@ -38,6 +38,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/', include('mpesa_api.urls')),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 
 ]
