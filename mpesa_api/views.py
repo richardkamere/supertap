271f05c3ea99eb3306e18808ce8c5751d26169f0
Richard <richardkamere20@gmail.com>
2022-01-07 11:01:00 +0300

    version 0.1.7
    failed reasons

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -77,13 +77,14 @@ def lipa_na_mpesa_online(request):
         "PartyA": stkRequest['phoneNumber'],  # replace with your phone number to get stk push
         "PartyB": LipanaMpesaPpassword.Business_short_code,
         "PhoneNumber": stkRequest['phoneNumber'],  # replace with your phone number to get stk push
-        "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+        "CallBackURL": "https://cfc6-197-254-46-90.ngrok.io/api/v1/c2b/confirmation",
         "AccountReference": stkRequest['merchantName'],
         "TransactionDesc": "PESAPAL SABI"
     }
 
     response = requests.post(api_url, json=request, headers=headers)
 
+    print(response.text)
     if response.status_code == 200:
         stkRequestV1 = StkPushCalls(
             businessShortCode=request['BusinessShortCode'],
@@ -148,8 +149,8 @@ def register_urls(request):
     headers = {"Authorization": "Bearer %s" % access_token}
     options = {"ShortCode": LipanaMpesaPpassword.Test_c2b_shortcode,
                "ResponseType": "Completed",
-               "ConfirmationURL": "https://178.128.205.106/api/v1/c2b/confirmation",
-               "ValidationURL": "https://178.128.205.106/api/v1/c2b/validation"}
+               "ConfirmationURL": "https://cfc6-197-254-46-90.ngrok.io/api/v1/c2b/confirmation",
+               "ValidationURL": "https://cfc6-197-254-46-90.ngrok.io/api/v1/c2b/validation"}
     response = requests.post(api_url, json=options, headers=headers)
     return HttpResponse(response.text)
 
@@ -171,58 +172,8 @@ def validation(request):
 
 @csrf_exempt
 def confirmation(request):
-    global mpesaReceiptNumber, transactionDate
-
-    mpesa_body = request.body.decode('utf-8')
-    mpesa_payment = json.loads(mpesa_body)
-    mpesaPayment = mpesa_payment['Body']
-    stkCallback = mpesaPayment['stkCallback']
-
-    if stkCallback.__contains__('CallbackMetadata'):
-
-        callbackMetadata = stkCallback['CallbackMetadata']
-        merchantRequestId = stkCallback['MerchantRequestID']
-        checkoutRequestId = stkCallback['CheckoutRequestID']
-        items = callbackMetadata['Item']
-
-        print(items)
-
-        for item in items:
-            print(item)
-            name = item['Name']
-            if name == 'MpesaReceiptNumber':
-                mpesaReceiptNumber = item['Value']
-                break
-            if name == 'TransactionDate':
-                transactionDate = item['Value']
-                break
-
-        print(mpesaReceiptNumber)
-        print(transactionDate)
-
-        context = {
-            "ResultCode": stkCallback['ResultCode'],
-            "ResultDesc": stkCallback['ResultDesc']
-        }
-        # print(dict(context))
-        # push_service = FCMNotification(
-        #     api_key="AAAA7k0KRjg:APA91bFOQD0g8Vd4AwtMNO841wBcyv7UnS_AyebMcj2kYIJhflfBKNqC_y3gKsUBEi08MMtCyxIMEvN_SR0ulkNtjD9PYDkMoTRGBaPed7UjNAHvr6PxGIxVyrl9Xs23BED5ZgpMpTMQ")
-        # registration_id = "449376197dbb559"
-        # message_title = "Success"
-        # message_body = "Payment successful"
-        # result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title,
-        #                                            message_body=message_body)
-        #
-        # print(result)
-
-        return JsonResponse(dict(context))
-    else:
-        context = {
-            "ResultCode": stkCallback['ResultCode'],
-            "ResultDesc": stkCallback['ResultDesc']
-        }
-        print(dict(context))
-        return JsonResponse(dict(context))
+    confirmationText = json.loads(request.body)
+    print(confirmationText)
 
 
 @permission_classes((AllowAny,))
