0c388bdb2e17a9ae0e68ed72f87a75a97c9770fd
Richard <richardkamere20@gmail.com>
2022-01-11 12:02:33 +0300

    version 0.2.4
    Fixed autocheck data

--- mpesa_api/urls.py
+++ mpesa_api/urls.py
@@ -1,6 +1,6 @@
+from django.urls import path
+
 from . import views
-from rest_framework import routers
-from django.urls import path, include
 
 urlpatterns = [
     path('online/lipa', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
