a2b1b4a72497999fb304b7f92a9609880a167f4f
Richard <richardkamere20@gmail.com>
2022-01-14 13:43:25 +0300

    version 0.3.3
    handling callback

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -186,7 +186,7 @@ def c2b_confirmation(request):
     print(mpesa_payment['TransAmount'])
     print(mpesa_payment['BusinessShortCode'])
 
-    if StkPushCalls.objects.filter(phoneNumber=mpesa_payment['MSISDN'], amount=mpesa_payment['TransAmount'],
+    if not StkPushCalls.objects.filter(phoneNumber=mpesa_payment['MSISDN'], amount=mpesa_payment['TransAmount'],
                                    businessShortCode=mpesa_payment['BusinessShortCode'],
                                    paymentStatus="Success").order_by('-id')[0].exist():
         originalCall = \
